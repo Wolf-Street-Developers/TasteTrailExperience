15a2dd97a4a919c124d3441403f57f50b0a96cfe
maksimkins <maks.aleshkov.04@gmail.com>
2024-08-28 14:48:32 +0400

    Implement IIncrementLike and IDecrement to Feedback and MenuItem

--- TasteTrailExperience/src/TasteTrailExperience.Core/Feedbacks/Repositories/IFeedbackRepository.cs
+++ TasteTrailExperience/src/TasteTrailExperience.Core/Feedbacks/Repositories/IFeedbackRepository.cs
@@ -5,7 +5,8 @@ namespace TasteTrailExperience.Core.Feedbacks.Repositories;
 
 public interface IFeedbackRepository : IGetFilteredByIdAsync<Feedback, int>, IGetAsNoTrackingAsync<Feedback?, int>, IGetCountAsync, 
 IGetCountBySpecificationIdAsync<Feedback, int>, IGetByIdAsync<Feedback?, int>,
-ICreateAsync<Feedback, int>, IDeleteByIdAsync<int, int?>, IPutAsync<Feedback, int?>
+ICreateAsync<Feedback, int>, IDeleteByIdAsync<int, int?>, IPutAsync<Feedback, int?>, 
+IIncrementLikesAsync<Feedback, int>, IDecrementLikesAsync<Feedback, int> 
 {
     public Task<decimal> GetAverageRatingAsync(int venueId);
 }
