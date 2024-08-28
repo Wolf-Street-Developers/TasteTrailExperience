19afe36c60e8c228256cf38f5710c6a346917a95
maksimkins <maks.aleshkov.04@gmail.com>
2024-08-28 14:10:26 +0400

    Create IMenuItemLikeRepository

---
+++ TasteTrailExperience/src/TasteTrailExperience.Core/MenuItemLikes/Repositories/IMenuItemLikeRepository.cs
@@ -0,0 +1,8 @@
+using TasteTrailData.Core.Common.Repositories.Base;
+using TasteTrailData.Core.MenuItemLikes.Models;
+
+namespace TasteTrailExperience.Core.MenuItemLikes.Repositories;
+
+public interface IMenuItemLikeRepository : ICreateAsync<MenuItemLike, int>, IDeleteByIdAsync<int, int?>
+{
+}
